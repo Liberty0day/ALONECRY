e0fd50ef25197621b506287ac2ce17a45fa4d477
Liberty0day <liberty188888@gmail.com>
2022-01-28 19:29:21 +0100

    up

--- install.sh
+++ install.sh
@@ -5,3 +5,10 @@ sh /2_volume/volume.sh
 sh /3_encrypt/encrypt.sh
 sh /4_filesystem/filesystem.sh
 sh /5_system/system.sh
+sh /6_parameter/parameter.sh
+sh /7_core/core.sh
+sh /8_grub/grub.sh
+sh /9_encrypt_home/encrypt_home.sh
+sh /10_table/table.sh
+sh /11_user/user.sh
+sh /12_driver/driver.sh
