cefecf0688117114a2a934b6759e736547346a26
Liberty0day <liberty188888@gmail.com>
2022-01-28 22:49:12 +0100

    up

--- configs/11_user/user.sh
+++ configs/11_user/user.sh
@@ -1,10 +1,11 @@
 USERNAME=liberty
 PASSWORD=operator
+
+useradd -mG wheel -s /usr/bin/zsh -c "Liberty" $USERNAME
+echo -e "operator\noperator\n" | passwd liberty
+echo -e "operator\noperator\n" | passwd root
+sed -i '82 s/^#//' /mnt/etc/sudoers
 mkdir /home/$USERNAME
 chown liberty:liberty /home/liberty
-useradd -mG wheel -s /usr/bin/zsh -c "Liberty" $USERNAME
-echo $PASSWORD |passwd liberty
-echo $PASSWORD |passwd root
-sed -i '82 s/^#//' /mnt/etc/sudoers
 chroot /mnt /bin/zsh
-
+sed -i "/root ALL=(ALL) ALL/a ${USERNAME} ALL=(ALL) ALL" /etc/sudoers
--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
+#!/bin/bash
 cd configs
 
-sh ~/install_arch/configs/1_partition/partition.sh
-sh ~/install_arch/configs/2_volume/volume.sh
-sh ~/install_arch/configs/3_encrypt/encrypt.sh
-sh ~/install_arch/configs/4_filesystem/filesystem.sh
-sh ~/install_arch/configs/5_system/system.sh
-sh ~/install_arch/configs/6_parameter/parameter.sh
-sh ~/install_arch/configs/7_core/core.sh
-sh ~/install_arch/configs/8_grub/grub.sh
-sh ~/install_arch/configs/9_encrypt_home/encrypt_home.sh
-sh ~/install_arch/configs/10_table/table.sh
-sh ~/install_arch/configs/11_user/user.sh
-sh ~/install_arch/configs/12_driver/driver.sh
+sh ~/configs/1_partition/partition.sh
+sh ~/configs/2_volume/volume.sh
+sh ~/configs/3_encrypt/encrypt.sh
+sh ~/configs/4_filesystem/filesystem.sh
+sh ~/configs/5_system/system.sh
+sh ~/configs/6_parameter/parameter.sh
+sh ~/configs/7_core/core.sh
+sh ~/configs/8_grub/grub.sh
+sh ~/configs/9_encrypt_home/encrypt_home.sh
+sh ~/configs/10_table/table.sh
+#sh ~/configs/11_user/user.sh
+#sh ~/configs/12_driver/driver.sh
+
