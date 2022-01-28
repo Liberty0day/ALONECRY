7829b3ea0c8895e22a906a25ecbcb9ced4ae05e1
Liberty0day <liberty188888@gmail.com>
2022-01-28 19:33:50 +0100

    dd

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 cd configs
 
-sh /1_partition/partition.sh
-sh /2_volume/volume.sh
-sh /3_encrypt/encrypt.sh
-sh /4_filesystem/filesystem.sh
-sh /5_system/system.sh
-sh /6_parameter/parameter.sh
-sh /7_core/core.sh
-sh /8_grub/grub.sh
-sh /9_encrypt_home/encrypt_home.sh
-sh /10_table/table.sh
-sh /11_user/user.sh
-sh /12_driver/driver.sh
+sh /install_arch/configs/1_partition/partition.sh
+sh /install_arch/configs/2_volume/volume.sh
+sh /install_arch/configs/3_encrypt/encrypt.sh
+sh /install_arch/configs/4_filesystem/filesystem.sh
+sh /install_arch/configs/5_system/system.sh
+sh /install_arch/configs/6_parameter/parameter.sh
+sh /install_arch/configs/7_core/core.sh
+sh /install_arch/configs/8_grub/grub.sh
+sh /install_arch/configs/9_encrypt_home/encrypt_home.sh
+sh /install_arch/configs/10_table/table.sh
+sh /install_arch/configs/11_user/user.sh
+sh /install_arch/configs/12_driver/driver.sh
