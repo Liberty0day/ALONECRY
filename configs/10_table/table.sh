c713b778ab66b38ea3eae61d12976fb4f4572712
Liberty0day <liberty188888@gmail.com>
2022-01-28 21:15:12 +0100

    up

--- configs/10_table/table.sh
+++ configs/10_table/table.sh
@@ -1,5 +1,3 @@
-  arch-chroot /mnt <<- EOF
-        set -o errexit
-        cat configs/0_files/fstab > /mnt/etc/fstab
-        cat configs/0_files/crypttab > /mnt/etc/crypttab
-EOF
+set -o errexit
+cat configs/0_files/fstab > /mnt/etc/fstab
+cat configs/0_files/crypttab > /mnt/etc/crypttab
--- configs/11_user/user.sh
+++ configs/11_user/user.sh
@@ -1,22 +1,10 @@
-  arch-chroot /mnt <<- EOF
-    set -o errexit
-    UID=1000
-    GID=1000
-    USERNAME=liberty
-    echo "Build start with USERNAME: $USERNAME UID: $UID GID: $GID" &&\
-    pacman -Sy &&\
-    : "Add user ${USERNAME} for yay install" &&\
-    groupadd -g ${GID} ${USERNAME} &&\
-    useradd -u ${UID} -g ${GID} -m -s /bin/bash ${USERNAME} &&\
-    passwd -d ${USERNAME} &&\
-    mkdir -p /etc/sudoers.d &&\
-    touch /etc/sudoers.d/${USERNAME} &&\
-    echo "${USERNAME} ALL=(ALL) ALL" > /etc/sudoers.d/${USERNAME} &&\
-    mkdir -p /home/${USERNAME}/.gnupg &&\
-    echo 'standard-resolver' > /home/${USERNAME}/.gnupg/dirmngr.conf &&\
-    chown -R ${USERNAME}:${USERNAME} /home/${USERNAME} &&\
-    mkdir /build &&\
-    chown -R ${USERNAME}:${USERNAME} /build
-EOF
+USERNAME=liberty
+PASSWORD=operator
+mkdir /home/$USERNAME
+chown liberty:liberty /home/liberty
+useradd -mG wheel -s /usr/bin/zsh -c "Liberty" $USERNAME
+echo $PASSWORD |passwd liberty
+echo $PASSWORD |passwd root
+sed -i '82 s/^#//' /mnt/etc/sudoers
 
 
