d5cbc9e53293d3237b35f40bcd9777c284c970e6
Liberty0day <liberty188888@gmail.com>
2022-01-30 23:13:56 +0100

    up

--- configs/4_filesystem/filesystem.sh
+++ configs/4_filesystem/filesystem.sh
@@ -1,6 +1,9 @@
-mkfs.xfs /dev/mapper/root
+#mkfs.xfs /dev/mapper/root
+mkfs.ext4 /dev/mapper/root
+
 mount /dev/mapper/root /mnt
-mkfs.fat -F32 /dev/sda1
+#mkfs.fat -F32 /dev/sda1
+mkfs.ext2 /dev/sda1
 mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
 
--- configs/8_grub/grub.sh
+++ configs/8_grub/grub.sh
@@ -2,5 +2,6 @@ mount -t efivarfs efivarfs /sys/firmware/efi/efivars
 arch-chroot /mnt        sed -i '/GRUB_CMDLINE_LINUX=/d' /etc/default/grub
 arch-chroot /mnt        sed -i 's/#GRUB_ENABLE_CRYPTODISK=y/GRUB_ENABLE_CRYPTODISK=y/' /etc/default/grub
 arch-chroot /mnt        /bin/echo GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=$(blkid -s UUID -o value /dev/mapper/archlvm-root):root root=/dev/mapper/root\" >> /mnt/etc/default/grub
-arch-chroot /mnt        grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=arch_grub --recheck
+#arch-chroot /mnt        grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=arch_grub --recheck
+arch-chroot /mnt        grub-install /dev/sda
 arch-chroot /mnt        grub-mkconfig -o /boot/grub/grub.cfg
