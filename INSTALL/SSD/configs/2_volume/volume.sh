c91c677ba7fc6de076b62a6b3e47b222b8d22d13
SuperCoconute <bezombes.corentin@gmail.com>
2022-03-17 19:01:17 +0100

    up ssd

--- INSTALL/SSD/configs/2_volume/volume.sh
+++ INSTALL/SSD/configs/2_volume/volume.sh
@@ -1,7 +1,7 @@
-pvcreate /dev/sda2
-vgcreate archlvm /dev/sda2
-lvcreate -L 10G -n root archlvm
+pvcreate /dev/sdb2
+vgcreate archlvm /dev/sdb2
+lvcreate -L 42G -n root archlvm
 lvcreate -L 2G -n swap archlvm
-lvcreate -L 1G -n tmp archlvm
+lvcreate -L 30G -n tmp archlvm
 lvcreate -l 100%FREE -n home archlvm
 
