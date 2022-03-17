b62f52998440c9caa9b88c1ddd50eb2c85f3ffaa
SuperCoconute <bezombes.corentin@gmail.com>
2022-03-18 00:33:40 +0100

    up

--- INSTALL/NVME/configs/4_filesystem/filesystem.sh
+++ INSTALL/NVME/configs/4_filesystem/filesystem.sh
@@ -1,9 +1,9 @@
-#mkfs.xfs /dev/mapper/root
-mkfs.ext4 /dev/mapper/root
+mkfs.xfs /dev/mapper/root
+# BIOS # mkfs.ext4 /dev/mapper/root
 
 mount /dev/mapper/root /mnt
-#mkfs.fat -F32 /dev/sda1
-mkfs.ext2 /dev/sda1
+mkfs.fat -F32 /dev/sda1
+# BIOS # mkfs.ext2 /dev/sda1
 mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
 
---
+++ INSTALL/SSD/configs/13_yay/yay.sh
@@ -0,0 +1,8 @@
+# after reboot
+# Switch user
+su - operator
+
+# Installation de yay
+git clone https://aur.archlinux.org/yay
+cd yay
+makepkg -sri
